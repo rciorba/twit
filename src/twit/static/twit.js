0afa9103b51e182e46fa348672250d97356df310
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2013-12-15 22:11:54 +0200

    add an error callback on getPosition
    turns out geolocation is actually broken in FFox

--- src/twit/static/twit.js
+++ src/twit/static/twit.js
@@ -6,6 +6,7 @@
     } else {
         var console = window.console;
     }
+    console.log("js loaded");
 
     var template = $("#twit_box_template").html();
     var container = $("#twit_box");
@@ -13,6 +14,13 @@
     var lon;
     var lat;
     var web_sock = null;
+
+    function message(msg, data){
+        console.log(msg);
+        if (data !== undefined){
+            console.debug(data);
+        }
+    }
 
     function render_all(data){
         var rendered = "";
@@ -26,9 +34,9 @@
     };
 
     function get_tweets(callback, forever){
-        console.log(lon);
         $.getJSON("/search/lonlat/"+lon+"+"+lat, function(data){
             callback(data);
+            console.log("got tweets")
         });
         if (forever){
             console.log(get_tweets);
@@ -36,20 +44,15 @@
         }
     };
 
-    function get_location() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(with_position);
-        }
-        console.log("geolocation is not supported");
-    }
-
     function main() {
         console.log([lon, lat]);
         get_tweets(render_all);
         if (web_sock !== null){
             web_sock.close();
         }
-        web_sock = new WebSocket("ws://"+window.location.hostname+":8001/"+lon+";"+lat+";.3");
+        var ws_url = "ws://"+window.location.hostname+":8001/"+lon+";"+lat+";.3";
+        console.log(ws_url);
+        web_sock = new WebSocket(ws_url);
         web_sock.onmessage = function(evt) {
             var tweet = $(_.template(template, $.parseJSON(evt.data)))
             tweet.hide().prependTo(container).fadeIn();
@@ -59,20 +62,34 @@
     };
 
     function with_position(position){
+        console.log("with_position")
         lon = position.coords.longitude;
         lat = position.coords.latitude;
         main(lon, lat);
-    }
+    };
 
     function manual_position(){
+        console.log("manual_position")
         lon = parseFloat($("#m_lon").val());
         lat = parseFloat($("#m_lat").val());
         main(lon, lat);
-    }
+    };
 
+    function get_location() {
+        console.log("get_location");
+        if ("geolocation" in navigator) {
+            console.log("geolocation supported");
+            var error_cb = _.partial(message, "geolocation error");
+            navigator.geolocation.getCurrentPosition(with_position, error_cb);
+        }
+        else{
+            console.log("geolocation is not supported");
+        }
+    };
 
     // get_tweets(render_all);
     get_location();
+    window.get_location = get_location;
     $("#m_butt").click(manual_position);
 
 
