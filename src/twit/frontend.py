b953388a1bc68571083b215384e01d51ef09093e
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2013-12-15 22:06:01 +0200

    allow running http and ws servers in separate processes

--- src/twit/frontend.py
+++ src/twit/frontend.py
@@ -11,21 +11,42 @@ import twit.search as search
 import twit.user_stream as user_stream
 
 
-def main():
-    static_app = paste.urlparser.StaticURLParser(
-        os.path.join(twit.__path__[0], "static"))
-    apps = paste.cascade.Cascade([static_app, search.app])
+def ws_server():
     ws_server = gevent.pywsgi.WSGIServer(
         ('', 8001), user_stream.web_socket_handler,
         handler_class=WebSocketHandler)
     ws_server.start()
+    gevent.spawn(user_stream.broadcast_tweets)
+    print "started ws_server"
+
+
+def http_server():
+    static_app = paste.urlparser.StaticURLParser(
+        os.path.join(twit.__path__[0], "static"))
+    apps = paste.cascade.Cascade([static_app, search.app])
     http_server = gevent.pywsgi.WSGIServer(
         ('', 8000), apps)
     http_server.start()
-    gevent.spawn(user_stream.broadcast_tweets)
-    while True:
-        gevent.sleep(1)
+    print "started http_server"
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+    print """alternate invocations:
+python -m twit.frontend ws  # starts websocket server only
+python -m twit.frontend http  # starts search + static server only
+"""
+    if len(sys.argv) > 1:
+        invocation = sys.argv[1]
+    else:
+        ws_server()
+        http_server()
+    if invocation == "ws":
+        ws_server()
+    elif invocation == "http":
+        http_server()
+    else:
+        print "illegal invocation %r" % invocation
+        sys.exit(1)
+    while True:
+        gevent.sleep(1)
--- src/twit/user_stream.py
+++ src/twit/user_stream.py
@@ -36,7 +36,6 @@ def parse_lon_lat(path):
 
 
 def web_socket_handler(environ, start_response):
-    print "called"
     web_sock = environ['wsgi.websocket']
     lon, lat, dist = parse_lon_lat(web_sock.path)
     user_stream(lon, lat, dist, web_sock)
