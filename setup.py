5432171208efb07eceb763f5f589bc5e795804a7
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2014-03-08 19:17:26 +0200

    extract requirements to text file + pin dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
+from os import path
 
+def parse_requirements(file_path=None):
+    if file_path is None
+        file_path = path.join(os.dirname(__file__), "REQUIREMENTS")
+    with open(file_path) as fd:
+        return [l.strip() for l in fd.xreadlines()]
 
 setup(
     name="twit",
     version="0.0.1",
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=["tweepy", "pyelasticsearch", "PyZMQ", "gevent", "paste",
-        "flask", "gevent-websocket"],
+    install_requires=parse_requirements(),
     package_data={
         '': ['*.txt'],
     },
