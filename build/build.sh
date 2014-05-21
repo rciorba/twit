a2f2b6bf14707c0128692f8d0c5ae8fcd9f887e4
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2014-05-21 15:15:34 +0300

    persistent logs

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,7 @@ if [ ! -d ./src/ ]; then
     git clone ../ ./src/ --depth=1
 fi
 
-DESCRIPTION=$(cd ./src/; git pull origin; git show HEAD -s --oneline)
+DESCRIPTION=$(cd ./src/; git pull origin > /dev/null; git show HEAD -s --oneline)
 echo "building for $DESCRIPTION"
 docker build -t twit .
 
